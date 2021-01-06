81f65addefc141a428bfa3cd9e7438af6cae9b19
davidashton0629 <77017046+davidashton0629@users.noreply.github.com>
2021-01-06 09:52:17 -0500

    Update typewriter-text.lua

--- typewriter-text.lua
+++ typewriter-text.lua
@@ -6,23 +6,57 @@ function splitText (str) -- create function to split string by each letter
 	return t
 end
 
-local t = { -- store table with text information
-	text = splitText("you came back for me..."), -- split text
-	curPrint = 1, -- printing location
-	toShow = "" -- text currently shown
-}
-local timeToWait = .1 -- how long you wait
-local timeWaited = 0 -- how long you've waited
+
+local typewriter = {}
+local typewriters = {}
+function typewriter:new(text, length, x, y)
+	local t = {}
+	t.text = splitText(text)
+	t.timeWaited = 0
+	t.timeToWait = length
+	t.curPrint = 1
+	t.toShow = ""
+	t.x = x
+	t.y = y
+	t.update = function(self, dt, v)
+		v.timeWaited = v.timeWaited + dt -- add to wait time
+		while v.timeWaited >= v.timeToWait and v.curPrint <= #v.text do
+			v.timeWaited = v.timeWaited - v.timeToWait
+			v.toShow = v.toShow .. v.text[v.curPrint] -- add to shown text
+			v.curPrint = v.curPrint + 1 -- increase printing location 
+		end
+	end
+	t.draw = function(self, v) love.graphics.print(v.toShow, v.x, v.y) end
+	typewriters[#typewriters + 1] = t
+	return t
+end
+
+function typewriter:update(dt)
+	for k, v in ipairs(typewriters) do v:update(dt, v) end
+end
+
+function typewriter:draw()
+	for k, v in ipairs(typewriters) do v:draw(v) end
+end
+
+return typewriter
+
+
+----- MAIN.lua
+--[[
+
+local typewriter = require("typewriter")
+
+local a = typewriter:new("hello", .5, 5, 10)
+local b = typewriter:new("world", .5, 40, 10)
+local c = typewriter:new("This is my text...", .25, 5, 50)
 
 function love.update(dt)
-	timeWaited = timeWaited + dt -- add to wait time
-	while timeWaited >= timeToWait and t.curPrint <= #t.text do
-		timeWaited = timeWaited - timeToWait
-		t.toShow = t.toShow .. t.text[t.curPrint] -- add to shown text
-		t.curPrint = t.curPrint + 1 -- increase printing location 
-	end
+	typewriter:update(dt)
 end
 
 function love.draw()
-	love.graphics.print(t.toShow, 10, 10) -- print shown text
+	typewriter:draw()
 end
+
+--]]
